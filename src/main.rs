0b5eeef8dca42faf02c1f5c65803ebe58c67c143
Alexander Potukar <alexander.potukar@eon.com>
2020-11-11 16:39:40 +0100

    WIP chapter 2

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,36 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
-
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        // resets buffer every time be re-declaring inside the loop
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // https://doc.rust-lang.org/std/io/trait.BufRead.html#method.read_line
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+        // expect == unwrap with a message
+        // let guess_as_int: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess_as_int: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    // expect == unwrap with a message
-    let guess_as_int: u32 = guess.trim().parse().expect("Please type a number!");
-
-    match guess_as_int.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess_as_int.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+
 }
