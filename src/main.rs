375d5b3a1346c56d896e3c5ae6f2a89bb9398e0b
Alexander Potukar <alexander.potukar@eon.com>
2020-11-11 17:35:08 +0100

    added unit test for app runner

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,27 @@
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+const MAX_POINTS: u32 = 100_000;
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
+    let mut y = 5 + MAX_POINTS;
+    println!("The value of y is: {}", y);
+    y = 6;
+    println!("The value of y is: {}", y);
 
-    loop {
-        println!("Please input your guess.");
+    // shadowing
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
 
-        // resets buffer every time be re-declaring inside the loop
-        let mut guess = String::new();
+    // https://stackoverflow.com/questions/4971286/javas-final-vs-cs-const
+    // https://stackoverflow.com/questions/14116003/difference-between-constexpr-and-const
+    let spaces = "   ";
+    println!("The value of spaces is: {}", spaces);
+    let spaces = spaces.len();
+    println!("The value of spaces is: {}", spaces);
 
-        // https://doc.rust-lang.org/std/io/trait.BufRead.html#method.read_line
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        println!("You guessed: {}", guess);
-        // expect == unwrap with a message
-        // let guess_as_int: u32 = guess.trim().parse().expect("Please type a number!");
-        let guess_as_int: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        match guess_as_int.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    // https://en.wikipedia.org/wiki/Two%27s_complement
+    // https://en.cppreference.com/w/cpp/language/ub
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("The value of guess is: {}", guess);
 
 }
